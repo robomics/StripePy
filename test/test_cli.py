72f2fc78de6f98f0687210cc4d8e37f41f547ff6
Bendik Berg <26233156+obikenobi23@users.noreply.github.com>
2024-10-28 11:54:39 +0100

    Add test suite for CLI parser input arguments

--- test/test_cli.py
+++ test/test_cli.py
@@ -8,6 +8,136 @@ import pytest
 from cli import parse_args as cli_parse_args
 
 
+class Test_configs_input:
+    # TODO: contact-map in output should be contact_map. Ask Andrea
+    def test_contact_map(self):
+        # Arrange
+        argument = "--contact-map"
+        variations = ("test\\data\\4DNFI6HDY7WZ.mcool", "test\\data\\4DNFIOTPSS3L.hic")
+
+        # Act
+        output = []
+        for variant in variations:
+            sys.argv = ["", variant, "10000"]
+            output.append(cli_parse_args()[0])
+
+        # Assert
+        assert output[0] == {
+            "contact-map": pathlib.Path("test\\data\\4DNFI6HDY7WZ.mcool"),
+            "resolution": 10_000,
+            "genomic_belt": 5_000_000,
+            "roi": None,
+        }
+
+        assert output[1] == {
+            "contact-map": pathlib.Path("test\\data\\4DNFIOTPSS3L.hic"),
+            "resolution": 10_000,
+            "genomic_belt": 5_000_000,
+            "roi": None,
+        }
+
+        # Cleanup
+        argument = None
+        variations = None
+        output = None
+        variant = None
+        sys.argv = [""]
+
+    def test_resolution(self):
+        # Arrange
+        argument = "resolution"
+        variations = (
+            1000,
+            2000,
+            5000,
+            10_000,
+            25_000,
+            50_000,
+            100_000,
+            250_000,
+            500_000,
+            1_000_000,
+            2_500_000,
+            5_000_000,
+            10_000_000,
+        )
+
+        # Act
+        output = []
+        for variant in variations:
+            sys.argv = ["", "test\\data\\4DNFI6HDY7WZ.mcool", str(variant)]
+            output.append(cli_parse_args()[0])
+
+        # Assert
+        for testNr in range(len(output)):
+            assert output[testNr] == {
+                "resolution": variations[testNr],
+                "contact-map": pathlib.Path("test\\data\\4DNFI6HDY7WZ.mcool"),
+                "genomic_belt": 5_000_000,
+                "roi": None,
+            }
+
+        # Cleanup
+        argument = None
+        variations = None
+        output = None
+        variant = None
+        testNr = None
+        sys.argv = [""]
+
+    def test_genomic_belt(self):
+        # Arrange
+        argument = "--genomic-belt"
+        variations = tuple(range(10_000, 1_010_000, 10_000))
+
+        # Act
+        output = []
+        for variant in variations:
+            print("M" + str(variant) + "M")
+            sys.argv = ["", "test\\data\\4DNFI6HDY7WZ.mcool", "10000", argument, str(variant)]
+            output.append(cli_parse_args()[0])
+
+        # Assert
+        for testNr in range(len(output)):
+            assert output[testNr] == {
+                "genomic_belt": variations[testNr],
+                "contact-map": pathlib.Path("test\\data\\4DNFI6HDY7WZ.mcool"),
+                "resolution": 10_000,
+                "roi": None,
+            }
+        # Cleanup
+        argument = None
+        variations = None
+        output = None
+        variant = None
+        testNr = None
+        sys.argv = [""]
+
+    def test_roi(self):
+        # Arrange
+        argument = "--roi"
+        variations = "middle"
+
+        # Act
+        output = []
+        sys.argv = ["", "test\\data\\4DNFI6HDY7WZ.mcool", "10000", argument, variations]
+        output.append(cli_parse_args()[0])
+
+        # Assert
+        assert output[0] == {
+            "roi": "middle",
+            "contact-map": pathlib.Path("test\\data\\4DNFI6HDY7WZ.mcool"),
+            "resolution": 10_000,
+            "genomic_belt": 5_000_000,
+        }
+
+        # Cleanup
+        argument = None
+        variations = None
+        output = None
+        sys.argv = [""]
+
+
 class Test_configs_thresholds:
 
     def test_glob_pers_type(self):
