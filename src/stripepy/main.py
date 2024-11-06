ffbb544a3e96fc44623e55ac4fc73b9dc9317684
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-11-06 17:24:10 +0100

    Setup main logger

--- src/stripepy/main.py
+++ src/stripepy/main.py
@@ -1,8 +1,17 @@
+import logging
+
 from .cli import call, download, setup
+
+
+def _setup_logger(level: str):
+    fmt = "[%(asctime)s] %(levelname)s: %(message)s"
+    logging.basicConfig(level=level, format=fmt)
+    logging.getLogger().setLevel(level)
 
 
 def main():
     subcommand, args = setup.parse_args()
+    _setup_logger("INFO")  # TODO make tunable
 
     if subcommand == "call":
         return call.run(**args)
