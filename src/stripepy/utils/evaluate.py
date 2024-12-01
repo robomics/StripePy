5f9c95b298d0a58758285121b737cce13ae23fa4
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-12-01 01:29:36 +0100

    Bugfix

--- src/stripepy/utils/evaluate.py
+++ src/stripepy/utils/evaluate.py
@@ -20,7 +20,7 @@ from . import IO
 colors = ["#e76f51", "#f4a261", "#e9c46a", "#2a9d8f", "#669bbc"]
 
 
-@function.cache
+@functools.cache
 def _bed6_columns() -> List[str]:
     return ["chrom", "start", "end", "name", "score", "strand"]
 
