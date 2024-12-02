187934e0c2d98c40a69159c8e494b9db95a4af72
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-12-02 18:15:04 +0100

    Add integration test for stripepy call

---
+++ test/integration/test_stripepy_call.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2024 Roberto Rossini <roberroso@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+import pathlib
+
+import h5py
+import pytest
+
+from stripepy import main
+
+testdir = pathlib.Path(__file__).resolve().parent.parent
+
+
+@pytest.mark.end2end
+class TestStripePyCall:
+    @staticmethod
+    def test_stripepy_call(tmpdir):
+        testfile = testdir / "data" / "4DNFIOTPSS3L.hic"
+        resolution = 10_000
+
+        args = ["call", str(testfile), str(resolution), "--output-folder", str(tmpdir)]
+        main(args)
+
+        outfile = pathlib.Path(tmpdir) / testfile.stem / str(resolution) / "results.hdf5"
+
+        assert outfile.is_file()
+        assert h5py.File(outfile).attrs.get("format", "unknown") == "HDF5::StripePy"
