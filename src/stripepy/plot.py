ff09f216db2153244d454f2e018973fca54dc7e6
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-12-13 12:48:26 +0100

    Fix docstring

--- src/stripepy/plot.py
+++ src/stripepy/plot.py
@@ -136,23 +136,58 @@ def _format_ticks(ax: plt.Axes, x: bool = True, y: bool = True, rotation: int = 
         ax.tick_params(axis="x", rotation=rotation)
 
 
+def _find_seeds_in_RoI(
+    seeds: List[int], left_bound_RoI: int, right_bound_RoI: int
+) -> Tuple[npt.NDArray[int], List[int]]:
+    """
+    Select seed coordinates that fall within the given left and right boundaries.
+
+    Parameters
+    ----------
+    seeds: List[int]
+        a list with the seed coordinates
+    left_bound_RoI: int
+        left bound of the region of interest
+    right_bound_RoI: int
+        right bound of the region of interest
+
+    Returns
+    -------
+    Tuple[NDArray[int], List[int]]
+        a tuple consisting of:
+
+         * the indices of seed coordinates falling within the given boundaries
+         * the coordinates of the selected seeds
+    """
+
+    assert left_bound_RoI >= 0
+    assert right_bound_RoI >= left_bound_RoI
+
+    # Find sites within the range of interest -- lower-triangular:
+    ids_seeds_in_RoI = np.where((left_bound_RoI <= np.array(seeds)) & (np.array(seeds) <= right_bound_RoI))[0]
+    seeds_in_RoI = np.array(seeds)[ids_seeds_in_RoI].tolist()
+
+    return ids_seeds_in_RoI, seeds_in_RoI
+
+
 def hic_matrix(
     I: npt.NDArray,
     RoI: Tuple[int, int],
-    title: Union[str, None] = None,
+    title: Optional[str] = None,
     cmap="fruit_punch",
     log_scale: bool = True,
     compact: bool = False,
-    fig: Union[plt.Figure, None] = None,
-    ax: Union[plt.Figure, None] = None,
+    fig: Optional[plt.Figure] = None,
+    ax: Optional[plt.Axes] = None,
 ) -> Tuple[plt.Figure, plt.Axes]:
     """
     :param I:                   Hi-C matrix to be plotted as image and saved
     :param  RoI:                refers to the Region of Interest [RoI[0], RoI[1]]x[RoI[2], RoI[3]]
                                 (e.g., in genomic coordinates)
     :param title:               title to give to the image
-    :param compact:          if False, it adds axes ticks, color bars
-    :return:                    -
+    :param compact:             if False, it adds axes ticks, color bars
+    :param fig:                 figure to use for plotting
+    :param ax:                  axis to use for plotting
     """
     _register_cmaps()
 
