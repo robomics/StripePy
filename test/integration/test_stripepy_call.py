e1ca2b8efcaa428db517de52afe7b6d21ea82a2b
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-01-08 19:27:24 +0100

    Update integration test for stripepy call to cover --roi=middle

--- test/integration/test_stripepy_call.py
+++ test/integration/test_stripepy_call.py
@@ -5,9 +5,12 @@
 import pathlib
 
 import h5py
+import hictkpy
 import pytest
 
 from stripepy import main
+
+from .common import matplotlib_avail
 
 testdir = pathlib.Path(__file__).resolve().parent.parent
 
@@ -44,6 +47,42 @@ class TestStripePyCall:
             "--output-folder",
             str(tmpdir),
         ]
+        main(args)
+
+        outfile = pathlib.Path(tmpdir) / testfile.stem / str(resolution) / "results.hdf5"
+
+        assert outfile.is_file()
+        assert h5py.File(outfile).attrs.get("format", "unknown") == "HDF5::StripePy"
+
+    @staticmethod
+    def test_stripepy_call_with_roi(tmpdir):
+        testfile = testdir / "data" / "4DNFI9GMP2J8.mcool"
+        resolution = 10_000
+
+        chrom_size_cutoff = max(hictkpy.MultiResFile(testfile).chromosomes().values()) - 1
+
+        args = [
+            "call",
+            str(testfile),
+            str(resolution),
+            "--glob-pers-min",
+            "0.10",
+            "--loc-pers-min",
+            "0.33",
+            "--loc-trend-min",
+            "0.25",
+            "--output-folder",
+            str(tmpdir),
+            "--min-chrom-size",
+            str(chrom_size_cutoff),
+            "--roi",
+            "middle",
+        ]
+        if not matplotlib_avail():
+            with pytest.raises(ImportError):
+                main(args)
+            pytest.skip("matplotlib not available")
+
         main(args)
 
         outfile = pathlib.Path(tmpdir) / testfile.stem / str(resolution) / "results.hdf5"
