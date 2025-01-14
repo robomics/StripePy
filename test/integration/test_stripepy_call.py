b7ac276659ce594a0fe1be412c9ab33ade2ecc4e
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-01-14 10:36:08 +0100

    Update tests

--- test/integration/test_stripepy_call.py
+++ test/integration/test_stripepy_call.py
@@ -32,12 +32,16 @@ class TestStripePyCall:
 
     @staticmethod
     def test_stripepy_call(tmpdir):
+        tmpdir = pathlib.Path(tmpdir)
         testfile = testdir / "data" / "4DNFI9GMP2J8.mcool"
         resolution = 10_000
 
         chrom_sizes = hictkpy.MultiResFile(testfile).chromosomes()
         chrom_size_cutoff = sum(chrom_sizes.values()) // len(chrom_sizes)
 
+        output_file = tmpdir / f"{testfile.stem}.hdf5"
+        log_file = tmpdir / f"{testfile.stem}.log"
+
         args = [
             "call",
             str(testfile),
@@ -48,24 +52,31 @@ class TestStripePyCall:
             "0.33",
             "--loc-trend-min",
             "0.25",
-            "--output-folder",
-            str(tmpdir),
+            "--output-file",
+            str(output_file),
+            "--log-file",
+            str(log_file),
             "--min-chrom-size",
             str(chrom_size_cutoff),
         ]
         main(args)
 
-        outfile = pathlib.Path(tmpdir) / testfile.stem / str(resolution) / "results.hdf5"
+        assert output_file.is_file()
+        assert h5py.File(output_file).attrs.get("format", "unknown") == "HDF5::StripePy"
 
-        assert outfile.is_file()
-        assert h5py.File(outfile).attrs.get("format", "unknown") == "HDF5::StripePy"
+        assert log_file.is_file()
 
     @staticmethod
     def test_stripepy_call_with_roi(tmpdir):
+        tmpdir = pathlib.Path(tmpdir)
         testfile = testdir / "data" / "4DNFI9GMP2J8.mcool"
         resolution = 10_000
 
         chrom_size_cutoff = max(hictkpy.MultiResFile(testfile).chromosomes().values()) - 1
+
+        output_file = tmpdir / f"{testfile.stem}.hdf5"
+        log_file = tmpdir / f"{testfile.stem}.log"
+        plot_dir = tmpdir / "plots"
 
         args = [
             "call",
@@ -77,8 +88,12 @@ class TestStripePyCall:
             "0.33",
             "--loc-trend-min",
             "0.25",
-            "--output-folder",
-            str(tmpdir),
+            "--output-file",
+            str(output_file),
+            "--log-file",
+            str(log_file),
+            "--plot-dir",
+            str(plot_dir),
             "--min-chrom-size",
             str(chrom_size_cutoff),
             "--roi",
@@ -93,7 +108,8 @@ class TestStripePyCall:
             warnings.filterwarnings("ignore", category=UserWarning)
             main(args)
 
-        outfile = pathlib.Path(tmpdir) / testfile.stem / str(resolution) / "results.hdf5"
+        assert output_file.is_file()
+        assert h5py.File(output_file).attrs.get("format", "unknown") == "HDF5::StripePy"
 
-        assert outfile.is_file()
-        assert h5py.File(outfile).attrs.get("format", "unknown") == "HDF5::StripePy"
+        assert log_file.is_file()
+        assert plot_dir.is_dir()
