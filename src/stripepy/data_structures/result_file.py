a9c94617691d13abdcaf97cbdd8494856092ec92
rea1991 <52046472+rea1991@users.noreply.github.com>
2025-07-03 13:32:30 +0200

    Removing wrong indentation in some docstrings
    This was done to correctly generate the Python API Reference for
    StripePy documentation.

--- src/stripepy/data_structures/result_file.py
+++ src/stripepy/data_structures/result_file.py
@@ -26,41 +26,41 @@ class ResultFile(object):
 
     There are 3 main use cases:
 
-        - Open the file in read mode:
+    - Open the file in read mode:
 
-          .. code-block:: python
+    .. code-block:: python
 
-              with ResultFile("results.hdf5") as h5:
-                  ...
+      with ResultFile("results.hdf5") as h5:
+      ...
 
-        - Open file in write mode:
+    - Open file in write mode:
 
-          - If all data will be written to the file before the file is closed:
+      - If all data will be written to the file before the file is closed:
 
-            .. code-block:: python
+        .. code-block:: python
 
-                with ResultFile.create("results.hdf5", mode="w", ...) as h5:
-                    h5.write_descriptors(res1)
-                    h5.write_descriptors(res2)
-                    ...
-
-          - If the data will be added progressively:
-
-            .. code-block:: python
-
-                with ResultFile.create("results.hdf5", mode="a", ...) as h5:
-                    h5.write_descriptors(res1)  # not mandatory, it is also possible to create the
-                                                # file and close it immediately
+            with ResultFile.create("results.hdf5", mode="w", ...) as h5:
+                h5.write_descriptors(res1)
+                h5.write_descriptors(res2)
                 ...
-                with ResultFile.append("results.hdf5") as h5:
-                    h5.write_descriptors(res2)
-                    h5.write_descriptors(res3)
-                ...
-                with ResultFile.append("results.hdf5") as h5:
-                    h5.write_descriptors(res4)
-                    h5.finalize()  # IMPORTANT!
-                                   # Without the above line you'll get an error when trying to open
-                                   # the file in read mode
+
+      - If the data will be added progressively:
+
+        .. code-block:: python
+
+            with ResultFile.create("results.hdf5", mode="a", ...) as h5:
+                h5.write_descriptors(res1)  # not mandatory, it is also possible to create the
+                                            # file and close it immediately
+            ...
+            with ResultFile.append("results.hdf5") as h5:
+                h5.write_descriptors(res2)
+                h5.write_descriptors(res3)
+            ...
+            with ResultFile.append("results.hdf5") as h5:
+                h5.write_descriptors(res4)
+                h5.finalize()  # IMPORTANT!
+                               # Without the above line you'll get an error when trying to open
+                               # the file in read mode
 
     When opening or creating a ResultFile write or append mode, a context manager (e.g. with:) must be used
 
@@ -404,13 +404,13 @@ class ResultFile(object):
             name of the field to be fetched.
             Supported names:
 
-                * pseudodistribution
-                * all_minimum_points
-                * persistence_of_all_minimum_points
-                * all_maximum_points
-                * persistence_of_all_maximum_points
-                * geo_descriptors
-                * bio_descriptors
+            * pseudodistribution
+            * all_minimum_points
+            * persistence_of_all_minimum_points
+            * all_maximum_points
+            * persistence_of_all_maximum_points
+            * geo_descriptors
+            * bio_descriptors
         location: str
             location of the attribute to be registered. Should be "LT" or "UT"
 
