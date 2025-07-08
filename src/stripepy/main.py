b4284dc04872a9783f4f4736b4b89b067032f21b
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-07-08 19:38:36 +0200

    Refactor

--- src/stripepy/main.py
+++ src/stripepy/main.py
@@ -4,10 +4,8 @@
 
 import contextlib
 import importlib
-import os
-import platform
 import sys
-from typing import Dict, List, Optional, Tuple
+from typing import List, Optional
 
 
 def _setup_logger(subcommand: str, verbosity: str, kwargs):
@@ -21,162 +19,6 @@ def _setup_logger(subcommand: str, verbosity: str, kwargs):
         print_welcome_message=subcommand != "view",
         progress_bar_type=subcommand,
     )
-
-
-def _try_fetch_dep_metadata(req: str) -> Optional[Tuple[str, str]]:
-    try:
-        from importlib.metadata import version
-
-        from packaging.requirements import Requirement
-
-        dep = Requirement(req)
-        if dep.marker is not None and dep.marker != "all":
-            return None
-
-        if dep.name.startswith("opentelemetry"):
-            return None
-
-        return f"dependency.{dep.name}.version", version(dep.name)
-    except:  # noqa
-        return None
-
-
-def _collect_deps() -> Dict[str, str]:
-    from importlib.metadata import requires
-
-    deps_metadata = {}
-
-    for req in requires("stripepy-hic"):
-        metadata = _try_fetch_dep_metadata(req)
-        if metadata is not None:
-            deps_metadata[metadata[0]] = metadata[1]
-    return deps_metadata
-
-
-def _generate_telemetry_resource():
-    from opentelemetry.sdk.resources import (
-        HOST_ARCH,
-        PROCESS_RUNTIME_DESCRIPTION,
-        PROCESS_RUNTIME_NAME,
-        PROCESS_RUNTIME_VERSION,
-        SERVICE_NAME,
-        SERVICE_VERSION,
-        OsResourceDetector,
-        Resource,
-    )
-
-    from stripepy import __version__ as stripepy_version
-
-    os_res = OsResourceDetector(raise_on_error=False).detect()
-
-    res = Resource.create(
-        {
-            HOST_ARCH: platform.machine(),
-            PROCESS_RUNTIME_NAME: platform.python_implementation(),
-            PROCESS_RUNTIME_VERSION: platform.python_version(),
-            PROCESS_RUNTIME_DESCRIPTION: platform.python_compiler(),
-            SERVICE_NAME: "stripepy",
-            SERVICE_VERSION: stripepy_version,
-        }
-    )
-
-    deps = Resource.create(_collect_deps())
-
-    return os_res.merge(deps).merge(res)
-
-
-def _setup_telemetry(
-    subcommand: str,
-    debug: bool = False,
-    no_telemetry: bool = False,
-):
-
-    import structlog
-
-    if no_telemetry:
-        return contextlib.nullcontext()
-
-    if "STRIPEPY_NO_TELEMETRY" in os.environ:
-        no_telem_var = "STRIPEPY_NO_TELEMETRY"
-    elif "NO_TELEMETRY" in os.environ:
-        no_telem_var = "NO_TELEMETRY"
-    else:
-        no_telem_var = None
-
-    if no_telem_var is not None:
-        structlog.get_logger().debug(f"detected {no_telem_var} variable in environment: no telemetry will be collected")
-        return contextlib.nullcontext()
-
-    if not debug:
-        import logging
-
-        logging.getLogger("opentelemetry").setLevel(logging.CRITICAL)
-
-    structlog.get_logger().debug("setting up telemetry...")
-
-    try:
-        from opentelemetry import trace
-        from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
-            Compression,
-            OTLPSpanExporter,
-        )
-        from opentelemetry.sdk.trace import TracerProvider
-        from opentelemetry.sdk.trace.export import (
-            BatchSpanProcessor,
-            ConsoleSpanExporter,
-        )
-
-        if debug:
-            processor = BatchSpanProcessor(ConsoleSpanExporter())
-        else:
-            processor = BatchSpanProcessor(
-                OTLPSpanExporter(
-                    endpoint="https://stripepy-telemetry.paulsenlab.com:4319/v1/traces",
-                    timeout=5,
-                    compression=Compression.Gzip,
-                )
-            )
-
-        provider = TracerProvider(resource=_generate_telemetry_resource())
-        provider.add_span_processor(processor)
-        trace.set_tracer_provider(provider)
-
-        tracer = trace.get_tracer("stripepy")
-        structlog.get_logger().debug("successfully configured StripePy for telemetry collection")
-
-        return tracer.start_span(
-            subcommand,
-            record_exception=False,  # exceptions are recorded manually so that we can redact sensitive information
-            set_status_on_exception=False,
-        )
-
-    except Exception as e:  # noqa
-        structlog.get_logger().debug(f"failed to setup telemetry: no telemetry will be collected: {e}")
-        return contextlib.nullcontext()
-
-
-def _telemetry_set_exception(span, exc: Exception):
-    try:
-        from opentelemetry.trace import Status, StatusCode
-
-        span.set_status(Status(StatusCode.ERROR))
-        span.record_exception(exc, attributes={"exception.message": "", "exception.stacktrace": ""})
-    except:  # noqa
-        pass
-
-
-def _telemetry_set_success(span, exit_code: int):
-    try:
-        from opentelemetry.trace import Status, StatusCode
-
-        if exit_code == 0:
-            status = Status(StatusCode.OK)
-        else:
-            status = Status(StatusCode.ERROR)
-
-        span.set_status(status)
-    except:  # noqa
-        pass
 
 
 def _setup_matplotlib(subcommand: str, **kwargs):
@@ -207,13 +49,15 @@ def _dispatch_subcommand(subcommand: str, verbosity: str, **kwargs) -> int:
     Call the appropriate entrypoint
     """
 
+    from stripepy.cli import telemetry
+
     telem_span = kwargs.get("telem_span")
 
     try:
         entrypoint = importlib.import_module(f"stripepy.cli.{subcommand}")
     except Exception as e:  # noqa
         # This should never happen
-        _telemetry_set_exception(telem_span, e)
+        telemetry.set_exception(telem_span, e)
         raise NotImplementedError from e
 
     try:
@@ -222,19 +66,19 @@ def _dispatch_subcommand(subcommand: str, verbosity: str, **kwargs) -> int:
         else:
             ec = entrypoint.run(**kwargs)
 
-        _telemetry_set_success(telem_span, ec)
+        telemetry.set_success(telem_span, ec)
 
         return ec
 
     except Exception as e:
-        _telemetry_set_exception(telem_span, e)
+        telemetry.set_exception(telem_span, e)
         raise
 
 
 def main(args: Optional[List[str]] = None, no_telemetry: bool = False) -> int:
     # It is important that stripepy is not imported in the global namespace to enable coverage
     # collection when using multiprocessing
-    from stripepy.cli import setup
+    from stripepy.cli import setup, telemetry
 
     # Parse CLI args
     try:
@@ -248,7 +92,7 @@ def main(args: Optional[List[str]] = None, no_telemetry: bool = False) -> int:
 
     with contextlib.ExitStack() as ctx:
         main_logger = ctx.enter_context(_setup_logger(subcommand, verbosity, kwargs))
-        telem_span = ctx.enter_context(_setup_telemetry(subcommand, no_telemetry=no_telemetry))
+        telem_span = ctx.enter_context(telemetry.setup(subcommand, no_telemetry=no_telemetry))
 
         try:
             _setup_matplotlib(subcommand, **kwargs)
